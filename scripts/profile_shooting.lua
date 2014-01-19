f90e22c5194b1ec2e4512547d2f6cd9e18e3a3b4
Matti Kariluoma <matti@kariluo.ma>
2014-01-19 16:26:10 -0600

    first test

--- scripts/profile_shooting.lua
+++ scripts/profile_shooting.lua
@@ -1,14 +1,14 @@
 --[[
 rem How fast can we trigger the shots? Download the jpegs afterwords
 rem and check their exif data.
-@title Delayed Single Shot
+@title Profile Shooting Speed
 
 @param n number of images
 @default a 5
 --]]
 
 repeat 
-	n -= 1
+	n = n - 1
 	
 	press("shoot_half")
 	repeat sleep(50) until get_shooting() == true
