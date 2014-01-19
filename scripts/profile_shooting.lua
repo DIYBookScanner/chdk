a3dc66eba499f3d568946b22af975aeb811c5267
Matti Kariluoma <matti@kariluo.ma>
2014-01-19 17:04:16 -0600

    try aflock and aelock, inspiration from http://chdk.wikia.com/wiki/HDR_Fast_Shooter

--- scripts/profile_shooting.lua
+++ scripts/profile_shooting.lua
@@ -1,6 +1,7 @@
 --[[
 rem How fast can we trigger the shots? Download the jpegs afterwords
 rem and check their exif data.
+rem http://chdk.wikia.com/wiki/HDR_Fast_Shooter
 @title Profile Shooting Speed
 
 @param n number of images
@@ -8,13 +9,24 @@ rem and check their exif data.
 --]]
 
 press("shoot_half")
+repeat sleep(10) until get_shooting() == true
+
+release("shoot_half")
+repeat sleep(10) until get_shooting() == false
+
+set_aflock(1)
+set_aelock(1)
 
 repeat 
 	n = n - 1
 	
-	repeat sleep(10) until get_shooting() == true
-	click("shoot_full")
+	press("shoot_full_only")
+	repeat sleep(10) until get_shooting() == true	
+	
+	release("shoot_full_only")
+	repeat sleep(10) until get_shooting() == false
 	
 	until n < 1
 
-release("shoot_half")
+set_aelock(0)
+set_aflock(0)
