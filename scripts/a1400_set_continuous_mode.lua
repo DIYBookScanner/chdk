835f352603473a69d0da5bb4b004ccd7bf9878aa
nafraf <nafraf@users.noreply.github.com>
2014-02-27 18:25:34 -0300

    Efficient way to set A1400 continuous mode

--- scripts/a1400_set_continuous_mode.lua
+++ scripts/a1400_set_continuous_mode.lua
@@ -1,54 +1,25 @@
 --[[
 @title Set a1400 to continuous mode over chdkptp
 ]]
+
 capmode=require("capmode")
 
--- try and set to continuous mode
-local c = (get_drive_mode() == 1)
-
-local r = 3 -- retry r times
-while not c and (r > 0) do
-	r = r - 1
-	rec,vid,mode = get_mode()
-	if not rec then
-		switch_mode_usb(1)
-		sleep(1000)
-		rec,vid,mode = get_mode()
-	end
-	if rec and (capmode.get_name() == "AUTO") then
-		-- written for a1400
-		click("up")
-		sleep(100)
-	end
-	if rec and (capmode.get_name() == "LIVE") then
-		-- written for a1400
-		click("set")
-		sleep(500)
-		click("right")
-		sleep(500)
-		click("up")
-		sleep(1000)
-		click("set")
-		sleep(500)
-	end
-	if rec and (capmode.get_name() == "P") then
-		-- written for a1400
-		click("set")
-		sleep(500)
-		click("down")
-		sleep(1000)
-		click("down")
-		sleep(1000)
-		click("down")
-		sleep(1000)
-		click("down")
-		sleep(1000)
-		click("right")
-		sleep(500)
-		click("down")
-		sleep(1000)
-		click("set")
-		sleep(500)
-	end
-	c = (get_drive_mode() == 1)
+function enter_shoot_mode()
+    if ( get_mode() == false ) then
+        set_record(1)
+        while ( get_mode() == false ) do sleep(100) end
+    end
+    sleep(1000)
 end
+
+--main program
+    if ( get_config_value(119) == 0 ) then 
+        printf("Failed : Lua Native Calls not enabled") 
+        return
+    end 
+
+    enter_shoot_mode()
+    call_event_proc("UI.CreatePublic")
+    capmode.set("P")
+    call_event_proc("PTM_SetCurrentItem",0x8002,2)
+    call_event_proc("PTM_SetCurrentItem",0x800D,1)
